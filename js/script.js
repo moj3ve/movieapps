12bc0729b84d00d2d754a8e06dc854ab1d38e4ba
ZiniTevi <56213781+ZiniTevi@users.noreply.github.com>
2020-04-02 00:16:01 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
 $(function() {
     "use strict";
     $('.monica__slider').slick({
-        arrows: false,
+        arrows: true,
         dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
         pauseOnFocus: false,
-        autoplaySpeed: 1500,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.monica__slider_youtube').slick({
+        arrows: true,
+        dots: true,
+        autoplay: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        pauseOnFocus: false,
         responsive: [
             {
                 breakpoint: 900,
