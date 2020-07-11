285989bf598dc15ec12c95a6818f176de17f8f3a
ZiniTevi <56213781+ZiniTevi@users.noreply.github.com>
2020-07-11 12:38:31 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -39,17 +39,17 @@ $(function() {
     });
     // $('#ios-link-1 > a.btn').attr('href', 'ds/zinitevi.html?r=' + new Date().getTime());
     $('a.btn-android').attr('href', $('a.btn-android').attr('href') + '?r=' + new Date().getTime());
-
-    $('#no-revoke-no-jailbreak > a.btn').click(function(e){
-        e.preventDefault();
-        $(this).find('i').removeClass('fa-apple').addClass('spinner-border text-light');
-        var that = this;
-        $.get("ds/link2.html?r=" + new Date().getTime(), function(data, status){
-            $(that).find('i').removeClass('spinner-border text-light').addClass('fa-apple');
-            window.location = data;
-        });
-        return false;
-    });
+    $('#no-revoke-no-jailbreak > a.btn').attr('href', $('#no-revoke-no-jailbreak > a.btn').attr('href') + '?r=' + new Date().getTime());
+    // $('#no-revoke-no-jailbreak > a.btn').click(function(e){
+    //     e.preventDefault();
+    //     $(this).find('i').removeClass('fa-apple').addClass('spinner-border text-light');
+    //     var that = this;
+    //     $.get("ds/link2.html?r=" + new Date().getTime(), function(data, status){
+    //         $(that).find('i').removeClass('spinner-border text-light').addClass('fa-apple');
+    //         window.location = data;
+    //     });
+    //     return false;
+    // });
 
     addToHomescreen();
 
